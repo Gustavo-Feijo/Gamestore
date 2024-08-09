b330c4409f20850f95c981ecfc1c7abd941be52c
Gustavo Feijó <gustavofeijo.dev@gmail.com>
2024-08-09 14:40:34 -0300

    CHANGE: Changed order list types and added grid layout.

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 import { fetchOrders } from "@/actions";
-import { Order } from "@/types";
+import { OrderData } from "@/types";
 import { useEffect, useState } from "react";
 import OrderCard from "./OrderCard";
+import { Button } from "@/components/ui/button";
 
 // Component that represents the list of orders from a given user.
 export default function OrderPage() {
   // Use state for handling the orders data, the amount of displayed orders and the "Show More" button.
-  const [orders, setOrders] = useState<Order[]>([]);
+  const [orders, setOrders] = useState<OrderData[]>([]);
   // Pseudo page, since we are using only one single page and appending the results.
   const [page, setPage] = useState<number>(0);
   const [hasMore, setHasMore] = useState<boolean>(true);
@@ -29,21 +30,24 @@ export default function OrderPage() {
     loadOrders();
   }, [page]);
 
-  console.log(orders);
   // Add one for the page.
   const handleMore = () => {
     setPage((prevPage) => prevPage + 1);
   };
 
   return (
-    <main className="flex justify-center items-center px-6 py-10 xl:px-72">
+    <main className="flex justify-center items-center px-6 py-10 xl:px-60">
       <div>
         {orders.length > 0 ? (
-          <div className="flex flex-col gap-4">
+          <div className="grid gap-4 lg:grid-cols-2 2xl:grid-cols-3">
             {orders.map((order, index) => (
               <OrderCard orderData={order} key={index} />
             ))}
-            {hasMore && <button onClick={handleMore}>Show More</button>}
+            {hasMore && (
+              <Button onClick={handleMore} variant={"outline"} className="h-50">
+                Show More
+              </Button>
+            )}
           </div>
         ) : (
           <div>No orders found.</div>
