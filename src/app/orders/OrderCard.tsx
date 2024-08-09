68c117dca9a9fe66841f12524b859fd13b7e8abd
Gustavo Feijó <gustavofeijo.dev@gmail.com>
2024-08-09 14:42:28 -0300

    ADD: Card for each order.

--- src/app/orders/OrderCard.tsx
+++ src/app/orders/OrderCard.tsx
@@ -1,22 +1,48 @@
-import { Order } from "@/types";
+import { buttonVariants } from "@/components/ui/button";
+import { OrderData } from "@/types";
 import Image from "next/image";
+import Link from "next/link";
+import { FaGamepad, FaMoneyBill } from "react-icons/fa";
 
-function OrderCard({ orderData }: { orderData: Order }) {
-  const orderCopy = orderData;
+function OrderCard({ orderData }: { orderData: OrderData }) {
   return (
-    <div className="h-40 bg-secondary">
-      <div className="flex">
-        {orderCopy.products.slice(0, 2).map((game, index) => (
-          <Image
-            src={game.game.image}
-            width={120}
-            height={160}
-            alt={game.game.image}
-            key={index}
-          />
-        ))}
+    <div className="flex flex-col bg-secondary rounded px-2 shadow-md shadow-secondary">
+      <div className="border-b border-foreground text-center">
+        Order Date: {orderData.createAt.toDateString()}
       </div>
-      {orderData.id}
+      <div className="h-44 flex justify-start px-2 items-center">
+        <Image
+          src={orderData.products[0].game.image}
+          width={120}
+          height={160}
+          alt={orderData.products[0].game.image}
+        />
+        <div className="flex flex-col h-full items-center justify-between p-2 min-w-52">
+          {orderData.paid ? (
+            <span className="text-green-300 text-lg">Paid</span>
+          ) : (
+            <span className="text-orange-300 text-lg">Awaiting Payment</span>
+          )}
+          <span className="flex items-center gap-2">
+            <FaGamepad className="text-yellow-300" />
+            Games: {orderData.products.length}
+          </span>
+          <span className="flex items-center gap-2">
+            <FaMoneyBill className="text-green-300" />
+            Total:
+            {orderData.products.reduce(
+              (prev, cur) => (prev += cur.game.price * cur.amount),
+              0
+            )}
+          </span>
+          <Link
+            href={`/orders/${orderData.id}`}
+            className={buttonVariants({ variant: "outline" })}
+          >
+            Open order
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
